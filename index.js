bf85c5c3daf1307cb90e09510ab6ff4329e480b4
joel burton <joeltburton@gmail.com>
2019-04-29 13:47:04 -0700

    basic server set up, whenever it's pinged, it will save a document in mongoDB with a timestamp.

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 3001;
+const app = express();
+const { Schema } = mongoose;
+
+
+
+// *** DATABASE & SCHEMA *** //
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('connected to db... 📀\n'));
+
+const Ping = mongoose.model(
+  'Ping',
+  new Schema({
+    created_at: { type: String, required: true },
+    source: String,
+  }),
+);
+
+
+
+
+// *** SERVER *** //
+
+
+app.use(cors());
+
+
+app.use('*', (req, res) => {
+  const created_at = new Date(Date.now()).toString();
+  const source = req.headers['user-agent'];
+  Ping.create({ created_at, source }, (err, ping) => {
+    if (err || ping.length === 0) {
+      console.log('there was an error saving to database...');
+    }
+    console.log(ping);
+  });
+  res.sendStatus(200);
+});
+
+
+
+
+
+app.listen(PORT, () => console.log(`\nServer listening at port ${PORT}... 📡`));
